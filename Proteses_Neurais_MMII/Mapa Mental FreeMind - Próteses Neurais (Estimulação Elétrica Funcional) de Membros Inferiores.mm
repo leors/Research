bf36c68b00af003473975ac534ba38618878cb1b
Leonardo Rodrigues da Silva <leonardo@biosmart.com.br>
2020-12-18 17:39:21 -0300

    Para a disciplina de Tecnologia Assistiva em Mobilidade
    Primeira versão para apresentação no final da disciplina de Tecnologia Assistiva em Mobilidade

---
+++ Proteses_Neurais_MMII/Mapa Mental FreeMind - Próteses Neurais (Estimulação Elétrica Funcional) de Membros Inferiores.mm
@@ -0,0 +1,187 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1603147765146" ID="ID_1923699960" MODIFIED="1603148709947">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Pr&#243;teses Neurais
+    </p>
+    <p style="text-align: center">
+      (Estimula&#231;&#227;o El&#233;trica Funcional)
+    </p>
+    <p style="text-align: center">
+      de Membros Inferiores
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1603148343679" ID="ID_438656769" MODIFIED="1603148370459" POSITION="right" TEXT="Requisitos">
+<node CREATED="1603150006009" ID="ID_466328703" MODIFIED="1603150017023" TEXT="Equipamentos">
+<node CREATED="1603148542766" ID="ID_1635089128" MODIFIED="1603148558412" TEXT="Eletroestimulador">
+<node CREATED="1603149072316" ID="ID_1900704790" MODIFIED="1603149143000" TEXT="Quantidade de canais pelo objetivo"/>
+<node CREATED="1603151023103" ID="ID_430917878" MODIFIED="1603749369123" TEXT="Portabilidade">
+<node CREATED="1603155434207" ID="ID_1049753722" MODIFIED="1603156033609" TEXT="Port&#xe1;til">
+<node CREATED="1603156040160" ID="ID_654279021" MODIFIED="1603156042827" TEXT="Bateria">
+<node CREATED="1603749296118" ID="ID_480049697" MODIFIED="1603749315415" TEXT="Considerar consumo"/>
+</node>
+<node CREATED="1603156044837" ID="ID_1291559345" MODIFIED="1603156049668" TEXT="Tamanho"/>
+</node>
+<node CREATED="1603749335689" ID="ID_446898293" MODIFIED="1603749355190" TEXT="&quot;de mesa&quot;"/>
+<node CREATED="1603750851615" ID="ID_903875477" MODIFIED="1603750868788" TEXT="Implant&#xe1;veis"/>
+</node>
+<node CREATED="1603155529502" ID="ID_1320416326" MODIFIED="1603155580274" TEXT="Equipamento que faz o processamento">
+<node CREATED="1603155585760" ID="ID_1587600418" MODIFIED="1603155592671" TEXT="Computador"/>
+<node CREATED="1603155595632" ID="ID_1719554858" MODIFIED="1603155606149" TEXT="Celular"/>
+<node CREATED="1603748335722" ID="ID_372186015" MODIFIED="1603748342649" TEXT="Dedicado"/>
+</node>
+</node>
+<node CREATED="1606163906166" ID="ID_1051800646" MODIFIED="1606163918692" TEXT="Eletrodos">
+<node CREATED="1606163920915" ID="ID_907592989" MODIFIED="1606163947255" TEXT="De superf&#xed;cie"/>
+<node CREATED="1606163949093" ID="ID_1221701996" MODIFIED="1606163956064" TEXT="Implantados">
+<node CREATED="1608313443304" ID="ID_915252085" MODIFIED="1608313470070" TEXT="Localiza&#xe7;&#xe3;o do eletrodo"/>
+</node>
+</node>
+<node CREATED="1603148565254" ID="ID_784764443" MODIFIED="1603149960976" TEXT="Sensores para realimenta&#xe7;&#xe3;o">
+<node CREATED="1603149530155" ID="ID_214299725" MODIFIED="1603149557112" TEXT="Eletrogoni&#xf4;metro">
+<node CREATED="1603750666248" ID="ID_650230367" MODIFIED="1603750683983" TEXT="Potenci&#xf4;metro"/>
+<node CREATED="1603750733717" ID="ID_860177970" MODIFIED="1608209744343" TEXT="Aceler&#xf4;metro / girosc&#xf3;pio"/>
+</node>
+<node CREATED="1603149559267" ID="ID_1569888301" MODIFIED="1603149566975" TEXT="MMG"/>
+<node CREATED="1603750685595" ID="ID_370324113" MODIFIED="1603750699718" TEXT="Centro de massa"/>
+<node CREATED="1603750706091" ID="ID_1791797432" MODIFIED="1603750732615" TEXT="For&#xe7;a / Torque"/>
+<node CREATED="1603152833490" ID="ID_1440233742" MODIFIED="1608210197583" TEXT="Leitor dos desejos do usu&#xe1;rio (listados na interface)"/>
+</node>
+<node CREATED="1603148738007" ID="ID_1204448655" MODIFIED="1603154975993">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Software com
+    </p>
+    <p>
+      algoritmo de controle
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1603149202909" ID="ID_822419687" MODIFIED="1607049477854" TEXT="Estrat&#xe9;gia de controle aplicado"/>
+<node CREATED="1603149430490" ID="ID_942211014" MODIFIED="1603149525549" TEXT="An&#xe1;lise dos sinais de entrada">
+<node CREATED="1603156873761" ID="ID_993289730" MODIFIED="1603156918176" TEXT="Posi&#xe7;&#xe3;o dos segmentos do corpo"/>
+<node CREATED="1603157179179" ID="ID_1793893638" MODIFIED="1603157206622" TEXT="Contra&#xe7;&#xe3;o/Fadiga da musculatura"/>
+<node CREATED="1603156923960" ID="ID_671470477" MODIFIED="1603156935825" TEXT="Inten&#xe7;&#xe3;o do usu&#xe1;rio"/>
+</node>
+<node CREATED="1603149152385" ID="ID_876699334" MODIFIED="1603149200120" TEXT="Programa&#xe7;&#xe3;o do objetivo a ser alcan&#xe7;ado"/>
+</node>
+<node CREATED="1603150725091" ID="ID_16047494" MODIFIED="1603150934488" TEXT="Sustenta&#xe7;&#xe3;o do tronco">
+<node CREATED="1603150536220" ID="ID_1634909351" MODIFIED="1603150587706" TEXT="Equipamento para suporte do corpo"/>
+<node CREATED="1603150967323" ID="ID_1430899118" MODIFIED="1603150987196" TEXT="Barras paralelas"/>
+<node CREATED="1603150987830" ID="ID_1160046417" MODIFIED="1603150994157" TEXT="Andador"/>
+<node CREATED="1603151007351" ID="ID_906357248" MODIFIED="1603151020858" TEXT="Muletas"/>
+</node>
+</node>
+<node CREATED="1604861656083" ID="ID_1452655100" MODIFIED="1608231798298" TEXT="Configura&#xe7;&#xf5;es">
+<node CREATED="1604861684555" ID="ID_1561274622" MODIFIED="1604861720827" TEXT="Defini&#xe7;&#xe3;o dos par&#xe2;metros iniciais de estimula&#xe7;&#xe3;o"/>
+<node CREATED="1608233540517" ID="ID_1028604611" MODIFIED="1608233650284" TEXT="Defini&#xe7;&#xe3;o dos limites de varia&#xe7;&#xe3;o dos par&#xe2;metros "/>
+</node>
+<node CREATED="1603150365588" ID="ID_1481637030" MODIFIED="1603150371922" TEXT="F&#xed;sicos">
+<node CREATED="1603150373274" ID="ID_786423432" MODIFIED="1603152371332" TEXT="Densidade &#xf3;ssea para suportar o peso do corpo e execu&#xe7;&#xe3;o de exerc&#xed;cios"/>
+<node CREATED="1603150458165" ID="ID_1886297358" MODIFIED="1603152046486">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Condi&#231;&#245;es de ficar em p&#233;
+    </p>
+    <p>
+      com suporte do tronco
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1603151429343" ID="ID_569309884" MODIFIED="1603151439598" TEXT="Hipotens&#xe3;o"/>
+<node CREATED="1603151445026" ID="ID_775226869" MODIFIED="1603151454217" TEXT="For&#xe7;a nos bra&#xe7;os"/>
+<node CREATED="1603151465736" ID="ID_24149176" MODIFIED="1603151503128" TEXT="Aus&#xea;ncia de encurtamento significativo"/>
+</node>
+</node>
+<node CREATED="1603150031136" ID="ID_1308062901" MODIFIED="1603150045720" TEXT="Cognitivos">
+<node CREATED="1603150047008" ID="ID_548702125" MODIFIED="1603150207000" TEXT="Percep&#xe7;&#xe3;o das a&#xe7;&#xf5;es que ser&#xe3;o executadas"/>
+</node>
+<node CREATED="1603150211645" ID="ID_959825286" MODIFIED="1603150251923" TEXT="Sensoriais">
+<node CREATED="1603150288628" ID="ID_800475597" MODIFIED="1603150318648" TEXT="Prefer&#xea;ncia por quem tem propriocep&#xe7;&#xe3;o"/>
+</node>
+</node>
+<node CREATED="1603148319656" ID="ID_1183283987" MODIFIED="1603148450587" POSITION="left" TEXT="Objetivos">
+<node CREATED="1603150817123" ID="ID_346785376" MODIFIED="1603150838803" TEXT="Fortalecimento muscular"/>
+<node CREATED="1603148407315" ID="ID_445478067" MODIFIED="1604516198683" TEXT="Ortostatismo / bipedesta&#xe7;&#xe3;o"/>
+<node CREATED="1603148453667" ID="ID_1432580649" MODIFIED="1604516064654" TEXT="Deambula&#xe7;&#xe3;o">
+<node CREATED="1603150761138" ID="ID_1889175640" MODIFIED="1603150791238" TEXT="Interno"/>
+<node CREATED="1603150792876" ID="ID_1997515066" MODIFIED="1603150803272" TEXT="Externo"/>
+</node>
+<node CREATED="1604515131303" ID="ID_219989127" MODIFIED="1604515188556" TEXT="Plasticidade Neural"/>
+<node CREATED="1606161152847" ID="ID_1279152492" MODIFIED="1606161181976" TEXT="Melhoria das condi&#xe7;&#xf5;es fisiol&#xf3;gicas"/>
+</node>
+<node CREATED="1603148265573" ID="ID_820878960" MODIFIED="1603746230998" POSITION="left" TEXT="Fun&#xe7;&#xe3;o / Funcionalidade">
+<node CREATED="1603148900849" ID="ID_1649428274" MODIFIED="1607559077697">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Contra&#231;&#227;o da musculatura para
+    </p>
+    <p style="text-align: center">
+      obter movimentos em malha aberta
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1604533082445" ID="ID_658596037" MODIFIED="1607559161029">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Algoritmo de controle para movimento
+    </p>
+    <p style="text-align: center">
+      utilizando malha fechada
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1603335068113" ID="ID_356279671" MODIFIED="1603745376869" TEXT="Leitura dos movimentos"/>
+<node CREATED="1603325068003" ID="ID_1323228923" MODIFIED="1603325137975" TEXT="Ler e analisar a atividade cerebral "/>
+</node>
+<node CREATED="1603152774944" ID="ID_30034902" MODIFIED="1603152786097" POSITION="left" TEXT="Interface">
+<node CREATED="1603152801555" ID="ID_1691333613" MODIFIED="1603155032872">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Leitura da
+    </p>
+    <p>
+      inten&#231;&#227;o do usu&#225;rio
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1603154096775" ID="ID_1615076444" MODIFIED="1603154115034" TEXT="EEG"/>
+<node CREATED="1603154211336" ID="ID_1757820059" MODIFIED="1603154220564" TEXT="EMG"/>
+<node CREATED="1603750502801" ID="ID_52362333" MODIFIED="1603750506437" TEXT="MMG"/>
+<node CREATED="1603154116274" ID="ID_1732701919" MODIFIED="1603750597968" TEXT="Acionador"/>
+</node>
+<node CREATED="1603751810223" ID="ID_1431805909" MODIFIED="1603751828920" TEXT="Feedback">
+<node CREATED="1603751831475" ID="ID_1662185937" MODIFIED="1603751846644" TEXT="Phi-t&#xe1;ctil"/>
+<node CREATED="1603753042815" ID="ID_752483778" MODIFIED="1603753050624" TEXT="Display"/>
+</node>
+</node>
+</node>
+</map>
